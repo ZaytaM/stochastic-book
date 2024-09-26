bd65a2a7569652669c6bb1a69f3c72067283b092
ZaytaM <54868127+ZaytaM@users.noreply.github.com>
2024-09-26 09:49:04 -0500

    asd

--- Scipt drafts/Poiss_Flip.R
+++ Scipt drafts/Poiss_Flip.R
@@ -1,8 +1,8 @@
-nsim=10
+nsim=10000
 sample_flip= function(lambda=1){
   N=rpois(1,lambda)
-  p=1/(N+1)
-  s=sample(c('H','T') , 1 ,replace=TRUE, prob = c(p,1-p))
-  return(s)
+  p=1/(N+1)^2
+  s=sample(c(1,0) , 1 ,replace=TRUE, prob = c(p,1-p))
+  return(c(N,s))
 }
-samples=replicate(nsim,sample_flip(lambda = 1))
+samples=data.frame(t(replicate(nsim,sample_flip(lambda=1))))
