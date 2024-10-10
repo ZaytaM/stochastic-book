ef28b9c02980a0b666536c0ef297fdcc4f86cdf6
ZaytaM <matias.delgadino@gmail.com>
2024-10-10 13:32:06 -0500

    New files

--- Scipt drafts/Hitting_Time_III.R
+++ Scipt drafts/Hitting_Time_III.R
@@ -5,25 +5,25 @@ simulate_random_walk <- function(horizon,p=0.5) {
     move <- sample(c(-1, 1), 1, prob=c(1-p,p))  # Randomly choose -1 or 1
     position <- position + move  # Update position
     if (position == 1) {
-      return(1)  # Return the 1 when hitting 1
+      return(step)  # Return the position when hitting 1
     }
   }
-  return(0)  # If horizon is reached without hitting return position
-}
-# This function is the indicator that tau_1 is less than the horizon
-l=4
-p=1/3
-prob=numeric(l)
-for (k in 1:l) {
-horizon=10^k
-trials=1000
-s=0
-for (i in 1:trials) {
-  s <- s+simulate_random_walk(horizon,p)
-}
-prob[k]=s/trials
+  return(horizon)  # If horizon is reached without hitting return position
 }
 
+l=4
+p=0.4
+E=numeric(l)
+# E is the expecation of min(tau_1,horizon)
+for (k in 1:l) {
+  horizon=10^k
+  trials=1000
+  s=0
+  for (i in 1:trials) {
+    s <- s+simulate_random_walk(horizon,p)
+  }
+  E[k]=s/trials
+}
 
 
 
